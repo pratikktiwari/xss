fb6d6f418378111931f6970affa38d474a7bfb73
pratikktiwari <ipratiktiwari@gmail.com>
2021-03-07 00:01:31 +0530

    Instagram new login Email

---
+++ phishing/instagram-email/README.md
@@ -0,0 +1,3 @@
+<h2>Instagram - Email</h2>
+<p>New login alert</p>
+![Email Preview](https://raw.githubusercontent.com/pratikktiwari/xss/main/phishing/instagram-email/instagram-email.png)
---
+++ phishing/instagram-email/index.php
@@ -0,0 +1,45 @@
+<?php $username = "pratikktiwari";?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New login to Instagram from Chrome Mobile</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<style type='text/css'>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
+		body{font-family: Roboto;font-weight: lighter;}
+	</style>
+</head>
+<body style='text-align: center;max-width: 600px;margin-left: auto;margin-right: auto;'>
+	<div style='width:95%;margin-left: auto;margin-right: auto;'>
+		<div style='display: flex;justify-content: space-between;background-color: #fff;align-items: center;'>
+			<div style='background-image: url(instagram_main_logo.jpg);height: 30px;width: 150px;background-size: contain;background-repeat: no-repeat;'>
+			</div>
+			<div style='background-image: url(pratikktiwari.jpg);height: 25px;width: 25px;border-radius: 50%;background-size: contain;background-repeat: no-repeat;'>
+			</div>
+		</div>
+		<h3 style='font-weight: normal;'>We Noticed a New Login,<br/> pratikktiwari</h3>
+		<div style='color:#969696;font-weight: normal;font-size: 16px;'>
+			We noticed a login from a device you don't<br/> usually use.
+		</div>
+		<br/><br/>
+		<div style='background-image: url(instagram_desk_cropped.jpg);height: 100px;margin-left: auto;margin-right: auto;width: 100px;background-size: contain;background-repeat: no-repeat;'>
+		</div>
+		
+		<h3 style='font-weight: normal;'>Ubuntu &bull; Firefox &bull; Ranchi, India</h3>
+		<div style='color:#969696;font-weight: normal;font-size: 16px;width:90%;margin-left: auto;margin-right: auto;'>
+			July 2 at 9:59 PM (IST)
+			<br/><br/>
+			If this was you, you can safely disregard this email. If this wasn't you, you can <a href="#" style='text-decoration: none;color: #435789;'>secure your account here.</a>
+			<br/><br/>
+			Learn more about keeping your account secure.
+		</div>
+		<br/><br/><br/>
+		<div style='border-top:0.5px solid #ddd;padding-top:10px;color:#969696;font-weight: normal;font-size: 12px;'>
+			Instagram From Facebook
+			<br/>
+			&copy; Instagram. Facebook Inc., 1601 Williw Road, Menlo Park, CA 94025
+		</div>
+	</div>
+
+</body>
+</html>
